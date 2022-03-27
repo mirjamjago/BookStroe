7396630a8bbeb460d4d6d9abc0ba26aa1308c573
mirjamjago <90257253+mirjamjago@users.noreply.github.com>
2022-03-27 10:38:15 +0300

    First input

---
+++ src/main/java/com/Book/BookStroe/BookStroeApplication.java
@@ -0,0 +1,13 @@
+package com.Book.BookStroe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStroeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStroeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Book/BookStroe/BookStroeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Book.BookStroe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStroeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
